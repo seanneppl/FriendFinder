9893fc127c54983fcf54ef555cb6003a7802f821
seanneppl <sean.e.neppl@gmail.com>
2018-07-23 20:43:35 -0500

    Update htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,14 +1,6 @@
-// 3. Your `htmlRoutes.js` file should include two routes:
-
-//    * A GET Route to `/survey` which should display the survey page.
-//    * A default, catch-all route that leads to `home.html` which displays the home page.
-
-
 const routes = require('express').Router();
 var path = require("path");
 
-
-// Basic route that sends the user first to the AJAX Page
 routes.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
 });
