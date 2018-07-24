db23ef2cc238fba9a6ef06ca8efb7bd6db74be4c
seanneppl <sean.e.neppl@gmail.com>
2018-07-23 20:34:24 -0500

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,3 @@
-// 5. You should save your application's data inside of `app/data/friends.js` as an 
-// array of objects. Each of these objects should roughly follow the format below.
-
 var surveys = [
 
     {
